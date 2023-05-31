65a855b4a435dc883db690029b376a403cc24c4f
Olatunde Olatunde <olatunde.oladunni.dev@gmail.com>
2023-05-31 13:18:29 +0100

    Just checking

--- utils.js
+++ utils.js
@@ -1,14 +1,25 @@
 const axios = require('axios')
 
 async function retrieveLatestEthPrice() {
-  const resp = await axios({
+  /*const resp = await axios({
     url: 'https://api.binance.com/api/v3/ticker/price',
     params: {
       symbol: 'ETHUSDT'
     },
     method: 'get'
   })
-  return resp.data.price
+
+  return resp.data.price*/
+  
+  const base = "https://api.coingecko.com/api/v3";
+
+  const getTokenPrice = await axios.get(
+    `${base}/simple/price?ids=${token}&vs_currencies=usd`
+  );
+
+  const tokenPrice = getTokenPrice.data.ethereum.usd;
+
+  return tokenPrice;
 }
 
 module.exports = {
